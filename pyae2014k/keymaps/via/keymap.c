1eedb0e218ec89cd09f58413049ea09cbc9c874b
Pyae2014K <pyae2014k@outlook.com>
2026-09-24 18:38:57 -0700

    Refactor keymap.c to simplify layers and OLED support

--- pyae2014k/keymaps/via/keymap.c
+++ pyae2014k/keymaps/via/keymap.c
@@ -2,9 +2,7 @@
 
 enum layers {
     _BASE = 0,
-    _NUMPAD,
-    _FUNCTION,
-    _ARROWS
+    _NUMPAD = 1
 };
 
 enum custom_keycodes {
@@ -47,33 +45,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P0,  KC_PENT, CYCLE_LYR
     ),
 
-    // Layer 2: Function
-    [_FUNCTION] = LAYOUT_ortho_3x4(
-        KC_F1,  KC_F2,  KC_F3,
-        KC_F4,  KC_F5,  KC_F6,
-        KC_F7,  KC_F8,  KC_F9,
-        KC_F10, KC_F11, TD(TD_F12_LAYER)
-    ),
-
-    // Layer 3: Arrows
-    [_ARROWS] = LAYOUT_ortho_3x4(
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_TRNS, CYCLE_LYR
-    )
-};
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    if (keycode == CYCLE_LYR && record->event.pressed) {
-        uint8_t current = get_highest_layer(layer_state);
-        layer_clear();
-        layer_on((current + 1) % 4);
-        return false;
-    }
-    return true;
-}
-
 #ifdef OLED_ENABLE
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     return OLED_ROTATION_0;
@@ -88,12 +59,6 @@ bool oled_task_user(void) {
         case _NUMPAD:
             oled_write_P(PSTR("Numpad\n"), false);
             break;
-        case _FUNCTION:
-            oled_write_P(PSTR("Function\n"), false);
-            break;
-        case _ARROWS:
-            oled_write_P(PSTR("Arrow\n"), false);
-            break;
     }
     return false;
 }
@@ -102,8 +67,6 @@ bool oled_task_user(void) {
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [_BASE]     = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_NUMPAD]   = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_FUNCTION] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
-    [_ARROWS]   = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+    [_NUMPAD]   = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) }
 };
 #endif
