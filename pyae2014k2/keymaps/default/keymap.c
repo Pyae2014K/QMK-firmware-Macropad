2992dd59b7392b73920bc9a5dcd902725d3ddfac
Pyae2014K <pyae2014k@outlook.com>
2026-09-25 16:17:40 -0700

    Update keymap.c to use TG instead of MO

--- pyae2014k2/keymaps/default/keymap.c
+++ pyae2014k2/keymaps/default/keymap.c
@@ -7,7 +7,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_MUTE,            LCTL(LSFT(KC_M)),  LCTL(LSFT(KC_D)),
         LCTL(KC_X),         LCTL(KC_C),        LCTL(KC_V),
         LGUI(LSFT(KC_S)),   LCTL(KC_Z),        LGUI(KC_V),
-        LCTL(LSFT(KC_ESC)), KC_DEL,            MO(1)
+        LCTL(LSFT(KC_ESC)), KC_DEL,            TG(1)
     ),
 
     // Layer 1: Numpad
@@ -15,7 +15,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_P7,  KC_P8,  KC_P9,
         KC_P4,  KC_P5,  KC_P6,
         KC_P1,  KC_P2,  KC_P3,
-        KC_P0,  KC_PENT, MO(0)
+        KC_P0,  KC_PENT, TG(0)
     ),
 };
 
